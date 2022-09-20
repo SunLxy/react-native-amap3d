821558e7c2099e7bc400aff8e842009a9b1c4a9f
SunLxy <1011771396@qq.com>
2022-09-20 13:48:41 +0800

    feat:新增ios监听定位

--- lib/ios/Modules/SdkModule.swift
+++ lib/ios/Modules/SdkModule.swift
@@ -3,18 +3,35 @@ class AMapSdk: NSObject {
   @objc static func requiresMainQueueSetup() -> Bool {
     false
   }
+  var locationManager;
+
+  @objc func supportedEvents(){
+    return ["AMapGeolocation" ]
+  }
 
   @objc func initSDK(_ apiKey: String) {
     AMapServices.shared().apiKey = apiKey
     MAMapView.updatePrivacyAgree(AMapPrivacyAgreeStatus.didAgree)
     MAMapView.updatePrivacyShow(AMapPrivacyShowStatus.didShow, privacyInfo: AMapPrivacyInfoStatus.didContain)
+
+    locationManager = AMapLocationManager()
+    locationManager.delegate = self
+    locationManager.locatingWithReGeocode = true
+  }
+
+  @objc func start() {
+    locationManager.startUpdatingLocation()
+  }
+
+  @objc func stop() {
+    locationManager.stopUpdatingLocation()
   }
 
   @objc func getVersion(_ resolve: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
     resolve("8.0.1")
   }
  // 计算距离
-  @objc func calculateLineDistance(_ latLng1:NSDictionary,latLng2:NSDictionary, resolve : RCTPromiseResolveBlock, reject : RCTPromiseRejectBlock) {
+  @objc func calculateLineDistance(_ latLng1:NSDictionary,latLng2:NSDictionary, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
     let point1 = MAMapPointForCoordinate(CLLocationCoordinate2D(latitude:(latLng1["latitude"] as! Double), longitude: (latLng1["longitude"] as! Double)))
     let point2 = MAMapPointForCoordinate(CLLocationCoordinate2D(latitude:(latLng2["latitude"] as! Double), longitude: (latLng2["longitude"] as! Double)))
     let distance = MAMetersBetweenMapPoints(point1,point2);
@@ -48,5 +65,48 @@ class AMapSdk: NSObject {
     let amapcoord = AMapCoordinateConvert(CLLocationCoordinate2D(latitude:(latLng["latitude"] as! Double), longitude: (latLng["longitude"] as! Double)), typeObj);
     var json = {["latitude":amapcoord.latitude,"longitude": amapcoord.longitude]}
     resolve(json)
+  }
+
+  func amapLocationManager(_ manager: AMapLocationManager!, didUpdate location: CLLocation!) {
+    self.sendEventWithName("AMapGeolocation",reGeocode(location))
+  }
+
+  func reGeocode(_ location: CLLocation!, reGeocode: AMapLocationReGeocode!) {
+    flag = AMapLocationDataAvailableForCoordinate(location.coordinate);
+    if(reGeocode){
+      return {[
+        "accuracy" : location.horizontalAccuracy,
+        "latitude" : location.coordinate.latitude,
+        "longitude" : location.coordinate.longitude,
+        "altitude" : location.altitude,
+        "speed" : location.speed,
+        "heading" : location.course,
+        "timestamp" : location.timestamp.timeIntervalSince1970 * 1000,
+        "isAvailableCoordinate": flag,
+
+        "address" : reGeocode.formattedAddress ? reGeocode.formattedAddress : "",
+        "country" : reGeocode.country ? reGeocode.country :"",
+        "province" : reGeocode.province ? reGeocode.province : "",
+        "city" : reGeocode.city ? reGeocode.city : "",
+        "district" : reGeocode.district ? reGeocode.district : "",
+        "cityCode" : reGeocode.citycode ? reGeocode.citycode : "",
+        "adCode" : reGeocode.adcode ? reGeocode.adcode : "",
+        "street" : reGeocode.street ? reGeocode.street : "",
+        "streetNumber" : reGeocode.number ? reGeocode.number : "",
+        "poiName" : reGeocode.POIName ? reGeocode.POIName : "",
+        "aoiName" : reGeocode.AOIName ? reGeocode.AOIName : "",
+      ]}
+    }else{
+      return {[
+        "accuracy": location.horizontalAccuracy,
+        "latitude": location.coordinate.latitude,
+        "longitude": location.coordinate.longitude,
+        "isAvailableCoordinate": flag,
+        "altitude": location.altitude,
+        "speed": location.speed,
+        "heading": location.course,
+        "timestamp": location.timestamp.timeIntervalSince1970 * 1000,
+      ]}
+    }
   }
 }
